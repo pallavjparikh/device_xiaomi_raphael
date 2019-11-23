af0fe246cd26845184221612efa59c57d1c79715
UtsavisGreat <utsavbalar1231@gmail.com>
2019-11-23 18:49:40 +0000

    raphael: parts: import devicesettings overlays
    from commit 6f0205a368206f1c487fd85b5a565b80d9da1ed2
    
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- parts/Android.mk
+++ parts/Android.mk
@@ -28,8 +28,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     vendor.xiaomi.hardware.motor-V1.0-java
 
 LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
+    $(LOCAL_PATH)/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
