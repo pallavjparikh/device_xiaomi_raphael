e1f61c261d5b5a10015f89c2f796d16a719d0fe0
pallavjparikh <45123301+pallavjparikh@users.noreply.github.com>
2019-12-17 21:00:59 +0530

    Update bootleg_raphael.mk

--- bootleg_raphael.mk
+++ bootleg_raphael.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common Bootleggers stuff.
-$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bootleg_raphael
+PRODUCT_NAME := aosp_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi K20 Pro
@@ -18,5 +18,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:10/QKQ1.190716.003/V10.4.8.0.QFKINXM:user/release-keys"
 
-REVENGEOS_BUILDTYPE := OFFICIAL
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
